3ae76de8e7a4f1a7c2a58e9d2cd8ed2746c3264b
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-07-14 17:40:57 -0400

    Create Header Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+
+  return (
+    <header>
+        <h2>TechVols</h2>
+        <ul>
+            <li>Volunteers</li>
+            <li>Organizations</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header

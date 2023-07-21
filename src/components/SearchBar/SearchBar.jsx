a59d994a6e4012573799fc9c2945b146954d7109
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-07-21 15:26:22 -0400

    Remove location searching

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,8 +5,8 @@ function SearchBar() {
   return (
     <form className="search-bar">
         <input className="search-key" type="text" name="key" placeholder="keyword"></input>
-        <label> | </label>
-        <input className="search-location" type="text" name="locations" placeholder="location"></input>
+        {/* <label> | </label>
+        <input className="search-location" type="text" name="locations" placeholder="location"></input> */}
         <input className="search-submit" type="submit" value="Search"></input>
     </form>
   )

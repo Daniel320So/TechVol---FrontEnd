8c48159135f753260232c3122a0f482a452ba01a
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-07-24 11:01:42 -0400

    Use Express to host the server

--- src/api/projects.js
+++ src/api/projects.js
@@ -2,7 +2,7 @@
 
 
 
-const apiPath = "http://localhost/";
+const apiPath = "http://104.225.142.52/";
 
 // Return an Object Array
 // Object => {id, user_id, title, description, status, committed_hour, start_Date, end_date, skills}
